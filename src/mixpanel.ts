2681586c65b5a2a144c0d0e0a50f245e1eddd2e7
Apoorva Mathur <aayaaymay@gmail.com>
2026-07-04 15:37:14 +0530

    Create mixpanel.ts

---
+++ src/mixpanel.ts
@@ -0,0 +1,11 @@
+import mixpanel from 'mixpanel-browser';
+
+mixpanel.init('ee6bd10d675d4eb08634ab00a9b36a5f', {
+  debug: true,
+  track_pageview: true,
+  persistence: 'localStorage'
+});
+
+export const trackEvent = (name: string, props?: any) => {
+  mixpanel.track(name, props);
+};
